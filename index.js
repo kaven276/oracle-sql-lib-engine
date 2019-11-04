b2f6f24c7b74a6372f7725d5d68f43ed2e7ae027
Li Yong <kaven276@vip.sina.com>
2019-11-04 10:53:42 +0800

    自动找启动项目根下的 .osqlrc.js配置

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const koaApp = require('./src/server.js');
+const path = require("path");
+
+const cfgFilePath = path.join(path.dirname(require.main.filename), ".osqlrc.js");
+global.cfg = require(cfgFilePath);
+
+const koaApp = require("./src/server.js");
 
 exports.koaApp = koaApp;
