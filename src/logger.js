e9e74f629c5f7805649c0e992d5c25e89fd699dc
Li Yong <kaven276@vip.sina.com>
2019-11-05 17:19:50 +0800

    upgrade to 0.0.7 no logger use ~/.pm2/logs

--- src/logger.js
+++ src/logger.js
@@ -2,11 +2,12 @@ const cfg = require('./cfg.js');
 const bunyan = require('bunyan');
 const path = require('path');
 
+const dirLog = cfg.dirLog || `${__dirname.split('/').slice(0, 3).join('/')}/.pm2/logs`;
 const logger = bunyan.createLogger({
   name: 'osql',
   streams: [{
     type: 'rotating-file',
-    path: path.join(cfg.dirLog, 'osql.log'),
+    path: path.join(dirLog, 'osql.log'),
     period: '1d', // daily rotation
     count: 31, // keep 3 back copies
   }],
