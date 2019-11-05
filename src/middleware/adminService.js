94d1c0cf78184b36ce358b6a1a5f12c9bcce9351
Li Yong <kaven276@vip.sina.com>
2019-11-05 11:17:35 +0800

    can monitor all pool stats

--- src/middleware/adminService.js
+++ src/middleware/adminService.js
@@ -1,18 +1,28 @@
 // 如果是管理服务
 // "$admin/pool/task" 打印 pool task 的连接池占用状态
 
+const poolMap = require('../buildAndKeepPools.js').registry;
+
+async function poolToStats(poolName) {
+  const pool = await oracledb.getPool(poolName);
+  return {
+    poolName,
+    connectionsInUse: pool.connectionsInUse,
+    connectionsOpen: pool.connectionsOpen,
+    poolMax: pool.poolMax,
+    free: pool.poolMax - pool.connectionsInUse,
+  };
+}
 module.exports = async function $admin(ctx, next) {
   const sects = ctx.path.split('/');
   if (sects[1] === '$admin') {
     // for /$admin/pool/task
     if (sects[2] === 'pool') {
-      const pool = await oracledb.getPool(sects[3]);
-      ctx.body = {
-        connectionsInUse: pool.connectionsInUse,
-        connectionsOpen: pool.connectionsOpen,
-        poolMax: pool.poolMax,
-        free: pool.poolMax - pool.connectionsInUse,
-      };
+      if (sects[3]) { // "$admin/pool/task"
+        ctx.body = await poolToStats(sects[3]);
+      } else { // $admin/pool
+        ctx.body = await Promise.all(Object.keys(poolMap).map(poolToStats));
+      }
     }
     return;
   }
