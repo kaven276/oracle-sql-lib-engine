01cbaa0c39c6221610af2fbbac5a9f9ad9727ef1
Li Yong <kaven276@vip.sina.com>
2019-11-06 20:46:35 +0800

    engine export startHttpServer for easy integration

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const koaApp = require("./src/server.js");
 
 exports.koaApp = koaApp;
+
+function startHttpServer() {
+  const PORT = Number(process.env.PORT) || 1520;
+  const http = require("http");
+
+  const server = http.createServer(koaApp.callback());
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log("oracle-sql-lib-engine server begin listening");
+  });
+}
+
+exports.startHttpServer = startHttpServer;
+
+if (require.main === module) {
+  // 直接用本项目启动场景
+  startHttpServer();
+}
