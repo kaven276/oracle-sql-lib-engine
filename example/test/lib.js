46baaab2a57f4e2770b666b59c554dfbd4126fbf
Li Yong <kaven276@vip.sina.com>
2019-11-25 17:05:11 +0800

    添加 qs ，测试支持查询串方式代理js参数

--- example/test/lib.js
+++ example/test/lib.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const qs = require('qs');
 
 axios.defaults.baseURL = 'http://127.0.0.1:1520';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -11,7 +12,14 @@ function check(result) {
   console.log(format(result.data));
 }
 
-function t(path, req = {}) {
+function t(path, req) {
+  if (req === undefined) {
+    const [pathname, query] = path.split('?');
+    if (query) {
+      path = pathname;
+      req = qs.parse(query);
+    }
+  }
   console.log('\nexecuting', path);
   console.log(format(req));
   return axios.post(path, req).then(check).catch(console.error);
