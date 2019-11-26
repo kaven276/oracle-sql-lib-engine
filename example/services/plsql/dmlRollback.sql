e96bc4f4aa268b310292fcc3ae5680e2cca51df6
Li Yong <kaven276@vip.sina.com>
2019-11-26 10:04:45 +0800

    plsql 范例模拟 oracle 错误和自定义升起异常

--- example/services/plsql/dmlRollback.sql
+++ example/services/plsql/dmlRollback.sql
@@ -2,9 +2,11 @@ declare
   v osql_main%rowtype;
 begin
   v.trade_id := :tradeId;
+  -- select 1/0 into v.trade_id from dual;
   delete from osql_main a where a.trade_id = v.trade_id
   returning a.cust_name into v.cust_name;
-  if v_cust_name = 'LiYong' then
-    raise_application_error(-20000, 'you can not delete cust LiYong');
+  if sql%rowcount = 0 then
+    raise_application_error(-20000, 'delete target not exist');
+    -- will automatically rollback the whole plsql as a sql sentence
   end if;
 end;
