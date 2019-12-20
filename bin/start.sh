efa4df5ceab627babd622839a4afc3fbf29dceb2
Li Yong <kaven276@vip.sina.com>
2019-12-20 14:10:51 +0800

    fix start/test scripts

---
+++ bin/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const server = require('../index.js');
+server.startHttpServer();
---
+++ bin/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+global.oracledb = require('oracledb');
+require('../src/servicesMap.js');
