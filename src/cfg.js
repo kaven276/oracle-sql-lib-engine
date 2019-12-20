c023ca51b03eda803307817b59f8f9a2a43cd358
Li Yong <kaven276@vip.sina.com>
2019-12-20 14:28:17 +0800

    tolerate no .osqlrc.js, default dirs under cwd

--- src/cfg.js
+++ src/cfg.js
@@ -1,7 +1,18 @@
 const path = require("path");
 // run server as lib project root directory
-const cfgFilePath = path.join(process.cwd(), ".osqlrc.js");
-const appConfig = require(cfgFilePath);
+const cwd = process.cwd();
+const cfgFilePath = path.join(cwd, ".osqlrc.js");
+let appConfig;
+try {
+  appConfig = require(cfgFilePath);
+} catch (e) {
+  appConfig = {
+    dirPools: `${cwd}/pools/`, // 从哪里扫描连接池配置目录，默认是 __dirname + '/pools'
+    dirServices: `${cwd}/services/`,
+    dirConverters: `${cwd}/converters/`,
+  };
+}
+
 const defaultConfig = {
   ConnectionTimeSlowThres: 5, // higher than this, log slow connection
   ExecutionTimeSlowThres: 50, // higher than this, log slow execution
